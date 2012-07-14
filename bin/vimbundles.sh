ee28a2de741d9c27bf5fd00eff3707b421e100f2
Jonathan Tron <jonathan@tron.name>
2012-07-14 16:34:04 +0200

    Add ervandew/supertab plugin

--- bin/vimbundles.sh
+++ bin/vimbundles.sh
@@ -42,5 +42,6 @@ get_bundle tomtom tcomment_vim
 get_bundle Lokaltog vim-powerline
 get_bundle nathanaelkane vim-indent-guides
 get_bundle skalnik vim-vroom
+get_bundle ervandew supertab
 
 vim -c 'call pathogen#helptags()|q'
